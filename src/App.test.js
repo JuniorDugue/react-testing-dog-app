fefeb91f41eeaf5f2e10bcaa50e3b37023ba0950
Junior Dugue <jrdugue@gmail.com>
2019-10-09 21:37:53 -0400

    pushing latest notes/updates
    pushing latest notes/updates

--- src/App.test.js
+++ src/App.test.js
@@ -6,9 +6,10 @@ import App from "./index";
 //will run after each test
 afterEach(rtl.cleanup);
 
-test('Render the app', ()=> {
-  const wrapper = rtl.render(<App/>)
-  wrapper.debug()
-})
+test("Render the app", () => {
+  const wrapper = rtl.render(<App />);
+  wrapper.debug(wrapper.queryByPlaceholderText('Image Count'))
+  wrapper.debug(wrapper.queryByText('The Dog Website'));
+});
 
 //go in terminal & run "npm test"
