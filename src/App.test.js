e87388cb5b243d1cef1030bc3120dd709a0d1a7c
Junior Dugue <jrdugue@gmail.com>
2019-10-09 21:13:16 -0400

    pushed latest notes
    pushed latest notes

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,14 @@
 import React from "react";
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import App from "./index";
+
+//will run after each test
+afterEach(rtl.cleanup);
+
+test('Render the app', ()=> {
+  const wrapper = rtl.render(<App/>)
+  wrapper.debug()
+})
+
+//go in terminal & run "npm test"
